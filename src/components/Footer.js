f48d58b619f3b6b149d5e46ba9b1e1b4468b0896
Koalia Sanders <76494729+koalias@users.noreply.github.com>
2021-07-12 23:59:14 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,6 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer class="h-10 font-black text-center absolute-bottom">Koalia's React Portfolio</footer>
+        <footer className="h-10 font-black text-center absolute-bottom">Koalia's React Portfolio</footer>
     )
 }
